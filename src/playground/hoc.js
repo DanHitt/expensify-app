5cfbb5258013d1d9a99c797aec9f6d7003bc182e
Dan Hitt <d.hitt424@gmail.com>
2018-04-25 16:31:56 -0600

    commit 2
    commit 2

--- src/playground/hoc.js
+++ src/playground/hoc.js
@@ -1,36 +1,42 @@
-
-
-// HOC = Higher Order Component - compontent that renders another componenent
+// Higher Order Component (HOC) - A component (HOC) that renders another component
+// Reuse code
+// Render hijacking
+// Prop manipulation
+// Abstract state
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Info = (props) => (
-    <div>
-        <h1>info</h1>
-        <p>this info is: {props.info}</p>
-    </div>
+  <div>
+    <h1>Info</h1>
+    <p>The info is: {props.info}</p>
+  </div>
 );
 
 const withAdminWarning = (WrappedComponent) => {
-    return (props) => (
-        <div>
-           {props.isAdmin &&  <p>This is private warning</p>}
-            <WrappedComponent {...props} />
-        </div>
-    );
+  return (props) => (
+    <div>
+      {props.isAdmin && <p>This is private info. Please don't share!</p>}
+      <WrappedComponent {...props} />
+    </div>
+  );
 };
 
-const requiredAuthentication = (WrappedComponent) => {
-    return (props) => (
-        <div>
-            { props.isAuthenticated ? <WrappedComponent {...props} /> : <p> Please log in </p>}
-        </div>
-    )
-}
+const requireAuthentication = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      {props.isAuthenticated ? (
+        <WrappedComponent {...props} />
+      ) : (
+          <p>Please login to view the info</p>
+        )}
+    </div>
+  );
+};
 
 const AdminInfo = withAdminWarning(Info);
-const AuthInfo = requiredAuthentication(Info);
+const AuthInfo = requireAuthentication(Info);
 
-// ReactDOM.render(<AdminInfo isAdmin={true} info="These are the details" />, document.getElementById('app'));
-ReactDOM.render(<AuthInfo isAuthenticated={true} info="These are the details" />, document.getElementById('app'));
+// ReactDOM.render(<AdminInfo isAdmin={true} info="There are the details" />, document.getElementById('app'));
+ReactDOM.render(<AuthInfo isAuthenticated={true} info="There are the details" />, document.getElementById('app'));
